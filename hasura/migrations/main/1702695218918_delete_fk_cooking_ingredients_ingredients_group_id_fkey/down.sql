b4fb37aefa610297affc7901508c3352ab7d5c2b
oleina <antholeinik@gmail.com>
2024-02-26 01:40:32 +0000

    OK MY GOD

--- hasura/migrations/main/1702695218918_delete_fk_cooking_ingredients_ingredients_group_id_fkey/down.sql
+++
@@ -1,5 +0,0 @@
-alter table "cooking"."ingredients"
-  add constraint "ingredients_group_id_fkey"
-  foreign key ("group_id")
-  references "public"."group"
-  ("id") on update cascade on delete cascade;
--- hasura/migrations/main/1702695218918_delete_fk_cooking_ingredients_ingredients_group_id_fkey/up.sql
+++
@@ -1,1 +0,0 @@
-alter table "cooking"."ingredients" drop constraint "ingredients_group_id_fkey";
