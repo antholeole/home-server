6114756c48877ec4651394d7db43fc18f4b3fdb4
oleina <oleina@google.com>
2025-01-20 13:21:28 -0800

    blame ignore format all

--- parts/devshell.nix
+++ parts/devshell.nix
@@ -48,6 +48,8 @@
         # https://www.reddit.com/r/tauri/comments/16tzsi8/tauri_desktop_app_not_rendering_but_web_does/
         export WEBKIT_DISABLE_DMABUF_RENDERER=1
         export WEBKIT_DISABLE_COMPOSITING_MODE=1
+
+        git config --local blame.ignoreRevsFile .git-blame-ignore-revs
       '';
     };
   };
