5f238d84948d7fcc61b88040c27171764b6fb5f4
anthony <antholeinik@gmail.com>
2025-06-27 23:30:50 -0700

    kustomize the cdk8s manifests!

--- cdk8s/default.nix
+++ cdk8s/default.nix
@@ -1,25 +1,75 @@
-{self,...}: {
+{self, ...}: {
   perSystem = {
     config,
     pkgs,
     ssot,
+    lib,
     ...
   }: {
-    packages.cdk8s = pkgs.buildNpmPackage (o: {
-      pname = "home-server-cdk8s";
-      version = "1.0.0";
-      src = "${self}/cdk8s/";
+    packages = rec {
+      manifests = let
+        # the manifests pre kustomize.
+        manifestsRaw = pkgs.buildNpmPackage (o: {
+          pname = "home-server-cdk8s";
+          version = "1.0.0";
 
-      nativeBuildInputs = [
-        pkgs.nodePackages_latest.cdk8s-cli
-      ];
+          # don't rebuild the manifests if we're just iterating on this file.
+          src = with lib.fileset;
+            toSource {
+              root = ./.;
+              fileset = fileFilter (file: ! file.hasExt "nix") ./.;
+            };
 
-      npmDepsHash = "sha256-yrdT5YTcc8FoL6TlMdSZsqpreg06DSsQ4rhxIxKcfmM=";
-      # makeCacheWritable = true;
-      # dontNpmBuild = true;
-      installPhase = ''
-        cp -r dist/ $out
-      '';
-    });
+          nativeBuildInputs = [
+            pkgs.nodePackages_latest.cdk8s-cli
+          ];
+
+          npmDepsHash = "sha256-2ifxpyhZ06sgX3Ac1YbjkeW2hNubihjQE9BSIioJIOc=";
+          installPhase = ''
+            mkdir -p $out/dist
+            cp -r dist/ $out/
+          '';
+        });
+
+        manifests = {
+          # attrset of { <image>: <image with tag> }
+          # e.g. { node: "node:21" }
+          manifests,
+        }:
+          pkgs.stdenvNoCC.mkDerivation {
+            name = "manifests";
+
+            dontUnpack = true;
+
+            nativeBuildInputs = [
+              pkgs.kustomize
+            ];
+
+            buildPhase = let
+              kustomizeParam = lib.attrsets.mapAttrsToList (image: tagged: "${image}=${tagged} ") manifests;
+            in ''
+              tmpdir=$(mktemp -d)
+
+              # todo this is bad how do we do it right
+              cp -r ${manifestsRaw}/dist $tmpdir/
+
+              cd $tmpdir/dist
+              chmod +rw ./*
+
+              kustomize edit set image ${lib.concatStrings kustomizeParam}
+              mkdir -p $out/
+              kustomize build $tmpdir/dist -o $out/
+            '';
+          };
+      in
+        lib.makeOverridable manifests {manifests = {};};
+
+      manifests-example = manifests.override {
+        manifests = {
+          node = "node:21";
+          redis = "redis:latest";
+        };
+      };
+    };
   };
 }
--- cdk8s/src/main.ts
+++ cdk8s/src/main.ts
@@ -1,15 +1,60 @@
-import type { Construct } from 'constructs';
-import { App, Chart, type ChartProps } from 'cdk8s';
+import type { Construct } from "constructs";
+import { App, Chart, type ChartProps } from "cdk8s";
+import * as kplus from "cdk8s-plus-32";
+import * as fs from "node:fs";
 
 export class MyChart extends Chart {
-  constructor(scope: Construct, id: string, props: ChartProps = { }) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props: ChartProps = {}) {
+		super(scope, id, props);
 
-    // define resources here
+		new kplus.Deployment(this, "FrontEnds", {
+			containers: [
+				{ image: "node" },
+				{ image: "redis" },
+			],
+		});
+	}
+}
+export class MyChart2 extends Chart {
+	constructor(scope: Construct, id: string, props: ChartProps = {}) {
+		super(scope, id, props);
 
-  }
+		new kplus.Deployment(this, "FrontEnds2", {
+			containers: [
+				{ image: "node" },
+				{ image: "redis" },
+			],
+		});
+	}
 }
 
-const app = new App();
-new MyChart(app, 'cdk8s');
+// override the synth function to also generate a kustomization.
+class KustomizeApp extends App {
+	synth() {
+		super.synth();
+
+		const allYamls = fs.readdirSync(app.outdir);
+		fs.writeFileSync(
+			`${app.outdir}/kustomization.yaml`,
+			`
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+
+resources:
+${allYamls
+	.filter((v) => v !== "kustomization.yaml")
+	.map((v) => `- ${v}`)
+	.join("\n")}
+`,
+		);
+	}
+}
+
+const app = new KustomizeApp();
+
+// put a kustomize file in there so we can update the image tags.
+
+new MyChart(app, "cdk8s");
+new MyChart2(app, "cdk8s2");
+
 app.synth();
