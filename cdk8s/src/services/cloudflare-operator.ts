b102f480b9cd9673eca37d56bacc91db4a0127af
anthony <antholeinik@gmail.com>
2025-08-17 14:19:00 -0700

    fix everything works again

--- cdk8s/src/services/cloudflare-operator.ts
+++ cdk8s/src/services/cloudflare-operator.ts
@@ -2,7 +2,7 @@ import { Chart } from "cdk8s";
 import { Kustomize } from "cdk8s-kustomize";
 import type { Construct } from "constructs";
 import { cloudflareOperator } from "../../kustomize/external-kustomize.json";
-import { ClusterTunnel } from "../../imports/networking.cfargotunnel.com";
+import { ClusterTunnelV1Alpha2 } from "../../imports/networking.cfargotunnel.com";
 
 export const tunnelRef = {
 	name: "k3s-cluster-tunnel",
@@ -10,7 +10,7 @@ export const tunnelRef = {
 };
 
 export class CloudflareOperatorChart extends Chart {
-	public readonly tunnelRef: ClusterTunnel;
+	public readonly tunnelRef: ClusterTunnelV1Alpha2;
 
 	constructor(scope: Construct) {
 		super(scope, "cloudflare-operator");
@@ -19,13 +19,13 @@ export class CloudflareOperatorChart extends Chart {
 			url: cloudflareOperator,
 		});
 
-		this.tunnelRef = new ClusterTunnel(this, tunnelRef.name, {
+		this.tunnelRef = new ClusterTunnelV1Alpha2(this, tunnelRef.name, {
 			spec: {
 				newTunnel: {
 					name: "home-server-tunnel",
 				},
 				cloudflare: {
-					email: "",
+					email: "antholeinik@gmail.com",
 					domain: "oleina.xyz",
 					accountId: "e0d74c227439ece29e62209d109ae43e",
 					secret: "cloudflare-tunnel-secrets",
